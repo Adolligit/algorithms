92f3b0843d149463921cb8dd4fd92cee7ec61217
Adolligit <adelson.onod@gmail.com>
2023-02-09 03:46:47 -0300

    feat/fix: (challenge_palindromes_recursive) entendi o que estava errando. Agora tudo esta passando nos testes

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,9 +1,10 @@
 def is_palindrome_recursive(word: str, low_index: int, high_index: int):
     """Vai retornar verdadeiro caso a palavra seja um palíndromo"""
-    try:
-        if low_index >= high_index:
-            return True
-        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
-    except Exception:
+    if len(word) <= 0:
         return False
+    if low_index >= high_index:
+        return True
+    if word[low_index] != word[high_index]:
+        return False
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
 
