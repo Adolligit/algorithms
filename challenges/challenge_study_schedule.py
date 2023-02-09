2ce77b3edf10a55710ac63e0fd4ac3305dec30e4
Adolligit <adelson.onod@gmail.com>
2023-02-09 01:07:17 -0300

    fix: (challenge_study_schedule) finalizando função do arquivo. Testada com êxito

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,14 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
+    """Retornará a quantidade de estudades no mesmo horários forncecido"""
     if not target_time:
         return None
-    
+
     students = 0
-    
+
     for entry, exit in permanence_period:
-        if entry is None or exit is None:
+        if type(entry) != int or type(exit) != int:
             return None
         elif entry <= target_time <= exit:
             students += 1
-    
+
     return students
