60ad138c11330d93bd1c37c4712d4f6d19eeb1a3
Adolligit <adelson.onod@gmail.com>
2023-02-09 04:11:17 -0300

    feat: (challenge_find_the_duplicate) Função implementada e testada com êxito

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,14 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    sorted_numbers = sorted(nums)
+
+    if len(sorted_numbers) < 2:
+        return False
+
+    for index in range(len(sorted_numbers) - 1):
+        if type(sorted_numbers[index]) != int or sorted_numbers[index] < 1:
+            return False
+
+        if sorted_numbers[index] == sorted_numbers[index + 1]:
+            return sorted_numbers[index]
+
+    return False
