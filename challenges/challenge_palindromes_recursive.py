67c7b9832bb18bfc9f9c4371cb208a7dc6218b02
Adolligit <adelson.onod@gmail.com>
2023-02-09 03:41:57 -0300

    chore: porque não consigo com try/catch?

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,9 @@
 def is_palindrome_recursive(word: str, low_index: int, high_index: int):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    """Vai retornar verdadeiro caso a palavra seja um palíndromo"""
+    try:
+        if low_index >= high_index:
+            return True
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    except Exception:
+        return False
+
