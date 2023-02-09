5a0b3565192fca9851141afaee9c37ac18e16bd0
Adolligit <adelson.onod@gmail.com>
2023-02-09 05:14:26 -0300

    chore: averiguando o problema

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -3,6 +3,8 @@ def is_palindrome_iterative(word):
         return False
 
     for i in range(len(word)):
-        print(i, word[i], word[-i -1])
+        if word[i] == word[-i -1]:
+            return True
+    return False
         
 is_palindrome_iterative('ana')
