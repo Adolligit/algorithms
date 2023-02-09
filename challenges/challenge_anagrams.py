b3164dc84da47e23240ecace084f32dd05f17e12
Adolligit <adelson.onod@gmail.com>
2023-02-09 02:30:34 -0300

    chore: (challenge_anagrams) iniciando implementação de lógica, utilizando 'quicksort'

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,12 @@
+def quicksort(list):
+    if not list:
+        return []
+    return (
+        quicksort([x for x in list[1:] if x < list[0]])
+            + [list[0]] +
+        quicksort([x for x in list[1:] if x >= list[0]])
+    )
+
+
 def is_anagram(first_string, second_string):
     """Meu código seŕa feito aqui"""
-    raise NotImplementedError
