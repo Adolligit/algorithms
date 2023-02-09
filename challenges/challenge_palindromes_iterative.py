4a45559a0757afc8a4f9be9f04cc53f738731deb
Adolligit <adelson.onod@gmail.com>
2023-02-09 05:23:59 -0300

    feat: (challenge_palindromes_interative) abordei uma outra lógica e parece que funciou melhor

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -2,9 +2,9 @@ def is_palindrome_iterative(word):
     if not word:
         return False
 
-    for i in range(len(word)):
-        if word[i] == word[-i -1]:
-            return True
-    return False
+    if word != word[::-1]:
+        return False
+    
+    return True    
         
 is_palindrome_iterative('ana')
