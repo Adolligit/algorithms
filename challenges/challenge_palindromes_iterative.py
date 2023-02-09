9a4f79319e6c1a1264f455401b84f1cb872866ea
Adolligit <adelson.onod@gmail.com>
2023-02-09 05:04:50 -0300

    chore: (challenge_palindromes_iterative) iniciando criação da função

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,7 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not word:
+        return False
+    for i in range(len(word)):
+        print(word[i])
+        
+is_palindrome_iterative('ana')
