0df5883f6b14ac42ecf39b02fd1a950c08632ec0
Adolligit <adelson.onod@gmail.com>
2023-02-09 00:58:04 -0300

    chore: (challenge_study_schedule) investingando o bug

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -2,4 +2,13 @@ def study_schedule(permanence_period, target_time):
     """Faça o código aqui."""
     if not target_time:
         return None
-    raise NotImplementedError
+    
+    students = 0
+    
+    for entry, exit in permanence_period:
+        if entry is None or exit is None:
+            return None
+        elif entry <= target_time <= exit:
+            students += 1
+    
+    return students
